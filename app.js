424e9bc85800297c75877595135224669d4294c6
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-06-03 15:59:01 +0800

    Created the errorController file which contains my error handler. Also created AppError class, which is a custom error class. This takes in a message and status code. Also has an additional attribute called isOperational, which is set to true. Only implemented error handling for unknown routes. Have not handled for other operational errors.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorController = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -22,10 +24,19 @@ app.use('/api/v1/users', userRouter);
 //Will reach here if other routers do not catch the request. Can handle uncaught requests here.
 //.all for all http methods, '*' is a wildcard to catch all other urls
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'Fail.',
-    message: `Cannot find ${req.originalUrl}`,
-  });
+  // res.status(404).json({
+  //   status: 'Fail.',
+  //   message: `Cannot find ${req.originalUrl}`,
+  // });
+  // const err = new Error(`Cannot find ${req.originalUrl}`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Cannot find ${req.originalUrl}`, 404)); //express assumes that whatever we pass into next is an error.
+  //then express will skip all other middlewares and send the error to the global errorhandling middleware
 });
+
+//Global error handler middleware function
+app.use(globalErrorController);
 
 module.exports = app;
