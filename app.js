ee92e75fd1324eead94ae37c5551535d6328b256
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-06-03 13:54:47 +0800

    Error handling for unhandles routes. Also installed ndb debugger.

--- app.js
+++ app.js
@@ -12,14 +12,20 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
-  next();
-});
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//Will reach here if other routers do not catch the request. Can handle uncaught requests here.
+//.all for all http methods, '*' is a wildcard to catch all other urls
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'Fail.',
+    message: `Cannot find ${req.originalUrl}`,
+  });
+});
 
 module.exports = app;
