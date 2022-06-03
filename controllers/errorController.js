0d7b2e8d02b6e788a8d3684367f22df2dd83555f
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2022-06-03 18:55:51 +0800

    Added error handling for database mongoose errors. Validator errors. Errors for development and production are different. For production it is more user friendly. Handle unhandledRejection and uncaughtExceptions in server.js as a final safety net.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,22 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFields = (err) => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value: ${value}. Please use another value.`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(' ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -8,19 +27,36 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  //operational error, send more details.
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    //programming error, unknown unpredicted error. Send vague message.
+    console.error('ERROR: ', err);
+
+    res.status(500).json({
+      status: 'Error.',
+      message: 'Something went wrong.',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+  // console.log(err);
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err }; //From comments.
+
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFields(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+    sendErrorProd(error, res);
   }
 };
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+//final safety net for syncrhonous uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught exception.');
+  console.log(err.message, err.name);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -19,6 +26,16 @@ mongoose
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
+});
+
+//final safety net for errors from async functions.
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled rejection.');
+  console.log(err);
+
+  server.close(() => {
+    process.exit(1); //end application. Exit with code 1 means error.
+  });
 });
